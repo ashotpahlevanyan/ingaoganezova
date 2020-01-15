f6dc6c6362427f7c76a9f761d99d87e6baa0cbd2
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2020-01-15 04:38:40 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1><a href="https://www.ingaoganezova.com">https://www.ingaoganezova.com</a></h1>
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'bootstrap'
+import 'bootstrap-select/dist/js/bootstrap-select.min';
+import 'stacktable.js/stacktable';
+import 'datatables-bootstrap/js/dataTables.bootstrap.min';
+import 'bootstrap-fileinput/js/fileinput.min';
+
+import './sass/main.scss'
+
+import './script';
+
