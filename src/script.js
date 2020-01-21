8e7ed126932f617991b2e833bf8dbb5b5d073eb2
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2020-01-21 18:49:10 +0400

    added some sections

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
     (function() {
         let requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
             function(callback) {
---
+++ src/scrollTop.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    var upToTop = $('.upToTop');
+    var scrollTopValue = $(window).scrollTop();
+    $(window).scroll(function() {
+        scrollTopValue = $(window).scrollTop();
+        if(scrollTopValue > 200) {
+            if(!upToTop.hasClass("show")) {
+                upToTop.addClass("show");
+            }
+        } else {
+            if(upToTop.hasClass("show")) {
+                upToTop.removeClass("show");
+            }
+        }
+    });
+    $(".upToTop .up").on("click", function(){
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+
+    $(".nav-item .nav-link").on("click", function(event){
+        event.preventDefault();
+        var href = $(this).attr("href");
+        console.log(href);
+        var scrollTopValue = $(href).offset().top;
+        console.log(scrollTopValue);
+        window.scrollTo({
+            top: scrollTopValue + 1,
+            behavior: "smooth"
+        });
+    });
+});
