fdc378feba92cc9dccea9ef28e40ef7b1774440a
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2020-01-31 14:47:26 +0400

    cleanup script

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    $("#address").on("click", function(event){
-        event.preventDefault();
-        event.stopPropagation();
-        //window.open('https://www.google.com/maps/place/7,+1+Nikoghayos+Adonts+St,+Yerevan+0014,+%D0%90%D1%80%D0%BC%D0%B5%D0%BD%D0%B8%D1%8F/@40.2098294,44.5263424,17z/data=!3m1!4b1!4m5!3m4!1s0x406aa2cbf31b85ad:0x77fbc66e3f6a4f82!8m2!3d40.2098294!4d44.5285311', '_blank');
-        $(".map").toggleClass("show");
-    });
+    // $("#address").on("click", function(event){
+    //     event.preventDefault();
+    //     event.stopPropagation();
+    //     //window.open('https://www.google.com/maps/place/7,+1+Nikoghayos+Adonts+St,+Yerevan+0014,+%D0%90%D1%80%D0%BC%D0%B5%D0%BD%D0%B8%D1%8F/@40.2098294,44.5263424,17z/data=!3m1!4b1!4m5!3m4!1s0x406aa2cbf31b85ad:0x77fbc66e3f6a4f82!8m2!3d40.2098294!4d44.5285311', '_blank');
+    //     $(".map").toggleClass("show");
+    // });
     /*(function() {
         let requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
             function(callback) {
