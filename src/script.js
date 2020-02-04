65aa8c275a56114c835d3522038de33d9f87015e
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2020-02-04 15:50:31 +0400

    before refactor

--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
         window.open('https://www.google.com/maps/place/7,+1+Nikoghayos+Adonts+St,+Yerevan+0014,+%D0%90%D1%80%D0%BC%D0%B5%D0%BD%D0%B8%D1%8F/@40.2098294,44.5263424,17z/data=!3m1!4b1!4m5!3m4!1s0x406aa2cbf31b85ad:0x77fbc66e3f6a4f82!8m2!3d40.2098294!4d44.5285311', '_blank');
         //$(".map").toggleClass("show");
     });
+
     var upToTop = $('.upToTop');
     var scrollTopValue = $(window).scrollTop();
     $(window).scroll(function() {
@@ -21,6 +22,7 @@ $(document).ready(function(){
             $(".header").removeClass("fixed");
         }
     });
+
     $(".upToTop .up").on("click", function(){
         window.scrollTo({
             top: 0,
@@ -30,18 +32,22 @@ $(document).ready(function(){
     var offsetValue = 0;
     $('body').scrollspy({ target: '#scrollspy', offset: offsetValue });
 
+
+
     // $(".nav-item .nav-link").on("click", function(event){
     //     event.preventDefault();
     //     var href = $(this).attr("href");
-    //     //console.log(href);
+    //
+    //
     //     var scrollTopValue = $(href).offset().top;
-    //     //console.log(scrollTopValue);
+    //     $('.navbar-collapse').collapse('hide');
     //     window.scrollTo({
     //         top: scrollTopValue + 1,
     //         behavior: "smooth"
     //     });
-    //     $(".nav-item").removeClass("active");
-    //     $(this).parent().addClass("active");
+    //     $("body").css({"padding-top": "96px"});
+    //     $(".nav-item .nav-link").removeClass("active");
+    //     $(this).addClass("active");
     // });
 
 
@@ -65,8 +71,11 @@ $(document).ready(function(){
                 // Add hash (#) to URL when done scrolling (default click behavior)
                 //window.location.hash = hash;
             });
+
+            $('.navbar-collapse').collapse('hide');
         }  // End if
     });
+
     // var scrollPos = $('body').find($(this).attr('href')).offset().top;
     // console.log(scrollPos);
     // $('body,html').animate({ scrollTop: scrollTopValue}, 500, function () {});
