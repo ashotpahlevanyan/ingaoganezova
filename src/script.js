0577e55ae52480b4dad055c004264ef65a1c28b7
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2020-02-04 19:05:46 +0400

    added htaccess

--- src/script.js
+++ src/script.js
@@ -6,10 +6,76 @@ $(document).ready(function(){
         //$(".map").toggleClass("show");
     });
 
-    var upToTop = $('.upToTop');
-    var scrollTopValue = $(window).scrollTop();
+    const upToTop = $('.upToTop');
+    let scrollTopValue = $(window).scrollTop();
+
+    let shift = $('.navbar').innerHeight();
+    const arr = ['#header', '#expertise', '#approach', '#services', '#results', '#about', '#contacts'];
+    const offsets = {};
+
+    // var offsetValue = 0;
+    // $('body').scrollspy({ target: '#scrollspy', offset: offsetValue });
+
+
+    function setOffsets() {
+        for(let item of arr) {
+            offsets[item] = $(item).position().top - shift;
+        }
+    }
+
+    function setActiveNavLink(hash) {
+        $('.nav-link').removeClass("active");
+        $(".nav-link[href='" + hash + "']").addClass("active");
+    }
+
+    function getActiveNavLink(value) {
+        for(let i = 0; i < arr.length - 1; i++) {
+            if(value >= offsets[arr[i]] && value < offsets[arr[i+1]]) {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+
+    $(".upToTop .up").on("click", function(){
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+
+    $("#scrollspy a").on('click', function(event) {
+        // offsetValue = $('.header').outerHeight();
+         shift = $('.navbar').innerHeight();
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+
+            const offsetScrollTopValue = $(hash).offset().top - shift;
+
+            $('html, body').animate({
+                scrollTop: offsetScrollTopValue
+            }, 800, function(){
+                //window.location.hash = hash;
+            });
+            setActiveNavLink(this.hash);
+            $('.navbar-collapse').collapse('hide');
+        }  // End if
+    });
+
     $(window).scroll(function() {
         scrollTopValue = $(window).scrollTop();
+        shift = $('.navbar').innerHeight();
+        setOffsets();
+        const hash = getActiveNavLink(scrollTopValue + shift);
+        setActiveNavLink(hash);
+
+
         if(scrollTopValue > 200) {
             $(".header").addClass("fixed");
             if(!upToTop.hasClass("show")) {
@@ -23,111 +89,25 @@ $(document).ready(function(){
         }
     });
 
-    $(".upToTop .up").on("click", function(){
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-    });
-    var offsetValue = 0;
-    $('body').scrollspy({ target: '#scrollspy', offset: offsetValue });
 
 
 
-    // $(".nav-item .nav-link").on("click", function(event){
-    //     event.preventDefault();
-    //     var href = $(this).attr("href");
-    //
-    //
-    //     var scrollTopValue = $(href).offset().top;
-    //     $('.navbar-collapse').collapse('hide');
-    //     window.scrollTo({
-    //         top: scrollTopValue + 1,
-    //         behavior: "smooth"
-    //     });
-    //     $("body").css({"padding-top": "96px"});
-    //     $(".nav-item .nav-link").removeClass("active");
-    //     $(this).addClass("active");
-    // });
-
-
-    $("#scrollspy a").on('click', function(event) {
-        offsetValue = $('.header').outerHeight();
-
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                //window.location.hash = hash;
-            });
-
-            $('.navbar-collapse').collapse('hide');
-        }  // End if
-    });
-
-    // var scrollPos = $('body').find($(this).attr('href')).offset().top;
-    // console.log(scrollPos);
-    // $('body,html').animate({ scrollTop: scrollTopValue}, 500, function () {});
-    //$(window).resize(resizeFunction);
     $(window).on("click", function(event) {
         if(!event.target.closest(".navbar")) {
             $('.navbar-collapse').collapse('hide');
         }
     });
 
+    $(window).resize(function() {
+        shift = $(".navbar").innerHeight();
+    })
 });
 
 $(window).on('load', function(){
-    var scrollTopValue = $(window).scrollTop();
+    const scrollTopValue = $(window).scrollTop();
     if(scrollTopValue > 200) {
         $(".header").addClass("fixed");
     } else {
         $(".header").removeClass("fixed");
     }
-
-    /*var $body   = $('body'),
-        $header = $('.header'),
-        offset  = $header.outerHeight();
-
-    // fix body padding (in case navbar size is different than the padding)
-    $body.css('padding-top', offset);
-    // Enable scrollSpy with correct offset based on height of navbar
-    $body.scrollspy({target: '#scrollspy', offset: offset });
-
-    // function to do the tweaking
-    function fixSpy() {
-        // grab a copy the scrollspy data for the element
-        var data = $body.data('activate.bs.scrollspy');
-        // if there is data, lets fiddle with the offset value
-        if (data) {
-            // get the current height of the navbar
-            offset = $navtop.outerHeight();
-            // adjust the body's padding top to match
-            $body.css('margin-top', offset);
-            // change the data's offset option to match
-            data.options.offset = offset;
-            // now stick it back in the element
-            $body.data('activate.bs.scrollspy', data);
-            // and finally refresh scrollspy
-            $body.scrollspy('refresh');
-        }
-    }
-
-    // Now monitor the resize events and make the tweaks
-    var resizeTimer;
-    $(window).resize(function() {
-        clearTimeout(resizeTimer);
-        resizeTimer = setTimeout(fixSpy, 200);
-    });*/
 });
